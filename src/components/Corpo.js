98ca68783e5a2da47ba296d2ad2969c94a042dda
demani.lukas@gmail.com <demani.lukas@gmail.com>
2021-12-03 18:48:07 +0000

    criando stories

---
+++ src/components/Corpo.js
@@ -0,0 +1,200 @@
+export default function Corpo() {
+  const imgsUsuarios = [
+    {imagem: "assets/img/9gag.svg", usuario: "9gag"},
+    {imagem: "assets/img/meowed.svg", usuario:"meowed"},
+    {imagem: "assets/img/barked.svg", usuario:"barked"},
+    {imagem: "assets/img/wawawicomics.svg",usuario:"wawawicomics"},
+    {imagem: "assets/img/respondeai.svg", usuario:"respondeai"},
+    {imagem: "assets/img/filomoderna.svg", usuario:"filomoderna"},
+    {imagem: "assets/img/memeriagourmet.svg", usuario:"memeriagourmet"},
+  ];
+
+  
+
+    return (
+        <div class="corpo">
+        <div class="esquerda">
+          <div class="stories">
+            
+            {imgsUsuarios.map(imgUsuario => <Story img={imgUsuario.imagem} usuario={imgUsuario.usuario}/>)}
+
+            <div class="setinha">
+              <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+          </div>
+
+          <div class="posts">
+            <div class="post">
+              <div class="topo">
+                <div class="usuario">
+                  <img src="assets/img/meowed.svg" />
+                  meowed
+                </div>
+                <div class="acoes">
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+
+              <div class="conteudo">
+                <img src="assets/img/gato-telefone.svg" />
+              </div>
+
+              <div class="fundo">
+                <div class="acoes">
+                  <div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+
+                <div class="curtidas">
+                  <img src="assets/img/respondeai.svg" />
+                  <div class="texto">
+                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="post">
+              <div class="topo">
+                <div class="usuario">
+                  <img src="assets/img/barked.svg" />
+                  barked
+                </div>
+                <div class="acoes">
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+
+              <div class="conteudo">
+                <img src="assets/img/dog.svg" />
+              </div>
+
+              <div class="fundo">
+                <div class="acoes">
+                  <div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+
+                <div class="curtidas">
+                  <img src="assets/img/adorable_animals.svg" />
+                  <div class="texto">
+                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="sidebar">
+          <div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+              <strong>catanacomics</strong>
+              Catana
+            </div>
+          </div>
+
+          <div class="sugestoes">
+            <div class="titulo">
+              Sugestões para você
+              <div>Ver tudo</div>
+            </div>
+
+            <div class="sugestao">
+              <div class="usuario">
+                <img src="assets/img/bad.vibes.memes.svg" />
+                <div class="texto">
+                  <div class="nome">bad.vibes.memes</div>
+                  <div class="razao">Segue você</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+              <div class="usuario">
+                <img src="assets/img/chibirdart.svg" />
+                <div class="texto">
+                  <div class="nome">chibirdart</div>
+                  <div class="razao">Segue você</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+              <div class="usuario">
+                <img src="assets/img/razoesparaacreditar.svg" />
+                <div class="texto">
+                  <div class="nome">razoesparaacreditar</div>
+                  <div class="razao">Novo no Instagram</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+              <div class="usuario">
+                <img src="assets/img/adorable_animals.svg" />
+                <div class="texto">
+                  <div class="nome">adorable_animals</div>
+                  <div class="razao">Segue você</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+              <div class="usuario">
+                <img src="assets/img/smallcutecats.svg" />
+                <div class="texto">
+                  <div class="nome">smallcutecats</div>
+                  <div class="razao">Segue você</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+          </div>
+
+          <div class="links">
+            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+          </div>
+
+          <div class="copyright">
+            © 2021 INSTAGRAM DO FACEBOOK
+          </div>
+        </div>
+      </div>
+    );
+}
+
+function Story(props){
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.imgem} />
+      </div>
+      <div class="usuario">
+        {props.usuario}
+      </div>
+    </div>
+  );
+}
